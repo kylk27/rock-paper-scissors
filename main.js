a9d4ff86143ac299904f78bb1a9e1f988c7177f9
kylekayaban27 <kylekayaban27@gmail.com>
2022-08-26 13:03:33 +0800

    Refactor code and add comments

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
-let array1 = ['rock', 'paper', 'scissors'];
+let playerSelection;
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
+//creates an array, generates a random item from that array and returns it
 function getComputerChoice() {
-    let choice = array1[Math.floor(Math.random()*array1.length)];
-    return choice;
-}
-
-function playRound(playerHand, compHand) {
-    compHand = getComputerChoice();
-    let result;
-    if ((playerHand == 'rock' && compHand == array1[0])||(playerHand == 'paper' && compHand == array1[1])||(playerHand == 'scissors' && compHand == array1[2])) {
-        result = `player: ${playerHand} computer: ${compHand} (it\'s a tie!)`;
-    } else if ((playerHand == 'rock' && compHand == array1[2])||(playerHand == 'paper' && compHand == array1[0])||(playerHand == 'scissors' && compHand == array1[1])) {
-        result = `player: ${playerHand} computer: ${compHand} (You won)`;
-        playerScore++;
-    } else {
-        result = `player: ${playerHand} computer: ${compHand} (You lost)`;
-        computerScore++;
-    };
-
+    const array = ['rock', 'paper', 'scissors'];
+    const result = array[Math.floor(Math.random()*array.length)];
     return result;
 };
 
-let compHand = getComputerChoice();
+//takes two parameters and returns the winner as well as keeping score
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt('Choose between rock, paper & scissors');
+    computerSelection = getComputerChoice();
 
+    if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
+        console.log(`Player: ${playerSelection} Computer: ${computerSelection} (It\'s a tie!)`);
+        console.log(`Score: Player(${playerScore})  Computer(${computerScore})`);
+
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        playerScore++;
+        console.log(`Player: ${playerSelection} Computer: ${computerSelection} (You win!)`);
+        console.log(`Score: Player(${playerScore})  Computer(${computerScore})`);
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        computerScore++;
+        console.log(`Player: ${playerSelection} Computer: ${computerSelection} (You lost!)`);
+        console.log(`Score: Player(${playerScore})  Computer(${computerScore})`);
+    };
+};
+/* 
+calls the function playRound() and allows you to 
+play until either you or opponent reaches a score 
+of 5, then prints the winner of the game
+*/
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerHand = `${prompt('choose between rock, paper and scissors')}`.toLowerCase();
-        console.log(playRound(playerHand, compHand));
-        console.log(`scoreboard: player (${playerScore}) computer(${computerScore})`);
+    for (let i = 0; (playerScore < 5 && computerScore <5); i++) {
+        playRound(playerSelection, computerSelection);
+    };
+
+    if (playerScore === 5) {
+        console.log('You won the game. Congratulations!');
+    } else {
+        console.log('You lost to the computer. Game over!');
     };
 };
 
